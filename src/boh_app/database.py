c8a11bdbf31b1f3b1b69cf29dfbc784a3e4d4e46
Philipp A. <flying-sheep@web.de>
2023-10-03 23:44:18 +0200

    be less talkative

--- src/boh_app/database.py
+++ src/boh_app/database.py
@@ -1,7 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+DEBUG = os.environ.get("DEBUG", "").lower() not in {"", "0", "false"}
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=DEBUG)
 
 Session = sessionmaker(bind=engine)
 session = scoped_session(Session)
