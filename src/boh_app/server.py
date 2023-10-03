0c67a8664c164eedbbaaef384d94107cc20a1b30
Desiree Davison <ded8393@gmail.com>
2023-10-03 18:04:33 +0200

    get all per table and by id

--- src/boh_app/server.py
+++ src/boh_app/server.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import mapper
 
 from .data.load_data import load_all
 from .database import Session, engine
-from .models import Aspect, Base
+from .models import Base
 from .serializers import setup_schema
 
 app = FastAPI()
@@ -20,6 +20,11 @@ def start_database():
     return session
 
 
+def get_model_by_name(name: str):
+    registry = Base.registry._class_registry
+    return registry.get(name.capitalize())
+
+
 session = start_database()
 
 
@@ -28,11 +33,21 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/aspect")
-async def get_all_aspect():
+@app.get("/{table}")
+async def get_all(table: str):
+    model = get_model_by_name(table)
     with session.begin():
-        data = session.query(Aspect).all()
-        resp = [Aspect.__marshmallow__().dump(d) for d in data]
+        data = session.query(model).all()
+        resp = [model.__marshmallow__().dump(d) for d in data]
+        return resp
+
+
+@app.get("/{table}/{id}")
+async def get_by_id(table: str, id: str | int):
+    model = get_model_by_name(table)
+    with session.begin():
+        data = session.query(model).get(id)
+        resp = model.__marshmallow__().dump(data)
         return resp
 
 
