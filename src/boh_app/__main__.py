04b39684fe17d3eff73231f8d4ddccde9aa7d5fa
Philipp A. <flying-sheep@web.de>
2023-10-08 16:43:58 +0200

    more flexible

--- src/boh_app/__main__.py
+++ src/boh_app/__main__.py
@@ -10,13 +10,16 @@ from .utils import gql_query
 def main(args: list[str] | None = None) -> None:
     if args is None:
         args = sys.argv[1:]
-    out_path = Path(args[0])
+    out_path = None if len(args) == 0 else Path(args[0])
 
     query = cast(LiteralString, get_introspection_query(descriptions=True))
     data = cast(IntrospectionQuery, gql_query(query))
     client_schema = build_client_schema(data)
-    with out_path.open("w") as f:
-        f.write(print_schema(client_schema))
+    schema_src = print_schema(client_schema)
+    if out_path is None:
+        print(schema_src)
+    else:
+        out_path.write_text(schema_src)
 
 
 if __name__ == "__main__":
