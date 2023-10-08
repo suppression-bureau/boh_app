b1fa72ee365816613186198a4ad5b43fbe89c27c
Philipp A. <flying-sheep@web.de>
2023-10-08 13:36:44 +0200

    works

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "node:path"
+import process from "node:process"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    // override defaults so index.html can be in src/front
+    root: "src/front",
+    cacheDir: "../../.vite_cache",
     build: {
-        rollupOptions: {
-            input: {
-                app: "src/front/index.html",
-            },
-        },
+        outDir: "../../dist",
     },
+    resolve: {
+        alias: { "/src/front": path.resolve(process.cwd(), "src/front") },
+    },
+    // app specific settings
     plugins: [react()],
 })
