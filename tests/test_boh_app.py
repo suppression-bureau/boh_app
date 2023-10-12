8ea68cb85d9c60dd5024973011269de66c235d3a
Desiree Davison <ded8393@gmail.com>
2023-10-12 17:57:42 +0200

    add PUT new entry test

--- tests/test_boh_app.py
+++ tests/test_boh_app.py
@@ -1,18 +1,32 @@
+from typing import Any
+
 from boh_app import models
 from boh_app.data.load_data import get_data
+
+
+def get_loaded_data(data: dict[str, Any], model: type[models.Base]) -> dict[str, Any]:
+    # data must be loaded through the serializer to get hybrid properties
+    # transient=True indicates we are Not touching the Database
+    serializer = model.__marshmallow__(transient=True, many=False)
+    model_data = serializer.load(data)
+    return serializer.dump(model_data)
 
 
 def test_post_no_fk(client):
     fake_data = {"id": "test_value"}
     result = client.post("aspect", json=fake_data)
-
-    model_serializer = models.Aspect.__marshmallow__(transient=True)
-    fake_model_instance = model_serializer.load(fake_data)
     assert result.status_code == 201
-    assert result.json() == model_serializer.dump(fake_model_instance)
+    assert result.json() == get_loaded_data(fake_data, models.Aspect)
 
 
-def test_put(client):
+def test_put_new_entry(client):
+    fake_data = {"id": "test_value"}
+    result = client.put(f"aspect/{fake_data['id']}", json=fake_data)
+    assert result.status_code == 200
+    assert result.json() == get_loaded_data(fake_data, models.Aspect)
+
+
+def test_put_replace_fk(client):
     skill_data = get_data("skill")
     og_skill_data = skill_data[0]
     new_data = {"wisdom_1": {"id": "test_value"}}
@@ -21,9 +35,4 @@ def test_put(client):
 
     result = client.put(f"/skill/{og_skill_data['id']}", json=updated_data)
     assert result.status_code == 200
-
-    # need to load the data through the serializer to get hybrid properties
-    # transient=True indicates we are Not touching the Database
-    model_serializer = models.Skill.__marshmallow__(transient=True)
-    full_updated_data = model_serializer.load(updated_data)
-    assert result.json() == model_serializer.dump(full_updated_data)
+    assert result.json() == get_loaded_data(updated_data, models.Skill)
