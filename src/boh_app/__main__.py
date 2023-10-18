4facb8cae0b1bd3a1eea3b95ecce75aabfb2880a
Philipp A. <flying-sheep@web.de>
2023-10-18 16:38:16 +0200

    Implement schema regeneration

--- src/boh_app/__main__.py
+++ src/boh_app/__main__.py
@@ -1,37 +1,84 @@
-import warnings
+import sys
+from asyncio.subprocess import create_subprocess_exec
+from functools import wraps
 from pathlib import Path
 from typing import Annotated, LiteralString, Optional, cast
 
+import anyio
 import typer
 from graphql import IntrospectionQuery, build_client_schema, get_introspection_query, print_schema
 
-from .database import DB_PATH
-from .utils import gql_query
+# Only imports with no dependencies here. E.g. no importing server, database, or db
+from .settings import DEBUG
+
+HERE = Path(__file__).parent
 
 app = typer.Typer(no_args_is_help=True)
 
 
+def run_async(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        async def coro_wrapper():
+            return await func(*args, **kwargs)
+
+        return anyio.run(coro_wrapper)
+
+    return wrapper
+
+
 @app.command()
-def schema(
+def api() -> None:
+    """Start the API server."""
+    import uvicorn
+
+    uvicorn.run(
+        "boh_app.server:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=DEBUG,
+    )
+
+
+@app.command()
+@run_async
+async def schema(
     out_path: Annotated[Optional[Path], typer.Argument()] = None,
     watch: Annotated[bool, typer.Option("-w", "--watch", help="Watch for changes")] = False,
 ) -> None:
     """Serialize generated schema to GraphQL schema language."""
+    from watchfiles import awatch
+
+    from .utils import gql_query
+
     query = cast(LiteralString, get_introspection_query(descriptions=True))
     data = cast(IntrospectionQuery, gql_query(query))
     client_schema = build_client_schema(data)
     schema_src = print_schema(client_schema)
-    if watch:
-        warnings.warn("TODO", stacklevel=1)
+
+    # print the schema once
     if out_path is None:
         print(schema_src)
     else:
         out_path.write_text(schema_src)
+    if not watch:
+        return
+
+    # watch for changes, rerun CLI with updated schema
+    args = ["-m", "boh_app", "schema"]
+    if out_path is not None:
+        args.append(str(out_path))
+    async for _change in awatch(HERE):
+        if out_path is not None:
+            typer.echo(f"Regenerating {out_path}")
+        await create_subprocess_exec(sys.executable, *args)
 
 
 @app.command()
 def empty_db() -> None:
     """Delete automatically generated database."""
+    from .database import DB_PATH
+
     if DB_PATH.is_file():
         DB_PATH.unlink()
 
