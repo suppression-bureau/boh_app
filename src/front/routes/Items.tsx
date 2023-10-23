9505fbfa9f5f78a61fc314592921dbbfbda08a85
Desiree Davison <ded8393@gmail.com>
2023-10-23 16:02:39 +0200

    update Item, add ItemReducer, filters in ItemView

--- src/front/routes/Items.tsx
+++ src/front/routes/Items.tsx
@@ -1,4 +1,5 @@
 import { CardContent } from "@mui/material"
+import { useReducer, useState } from "react"
 import { useQuery } from "urql"
 
 import Card from "@mui/material/Card"
@@ -35,34 +36,87 @@ const itemsQueryDocument = graphql(`
         }
     }
 `)
+const principles = [
+    "edge",
+    "forge",
+    "grail",
+    "heart",
+    "knock",
+    "lantern",
+    "moon",
+    "moth",
+    "nectar",
+    "rose",
+    "scale",
+    "sky",
+    "winter",
+]
+type ItemsFromQuery = types.ItemsQuery["item"]
+type ItemFromQuery = ItemsFromQuery[number]
 
-type ItemFromQuery = types.ItemsQuery["item"][number]
+interface ItemsProps {
+    filters?: {
+        known?: boolean
+        edge?: boolean
+        forge?: boolean
+        grail?: boolean
+        heart?: boolean
+        knock?: boolean
+        lantern?: boolean
+        moon?: boolean
+        moth?: boolean
+        nectar?: boolean
+        rose?: boolean
+        scale?: boolean
+        sky?: boolean
+        winter?: boolean
+        aspect?: string
+    }
+}
+type ItemsAction = ItemsActionInner | ItemsActionOuter
+type ItemsActionInner = never
+type ItemsActionOuter = { type: "filter"; filters: ItemsProps["filters"] }
+
+function filterItems(
+    filters: ItemsProps["filters"],
+    state: ItemsFromQuery[],
+): ItemsFromQuery[] {
+    let filtered_state = state
+    if (filters?.known) {
+        filtered_state = state.filter(({ known }) => known === true)
+    }
+    principles.forEach((principle) => {
+        if (filters?.[principle]) {
+            filtered_state = filtered_state.filter((item) => {
+                return item[principle] !== null
+            })
+        }
+    })
+    if (filters?.aspect) {
+        filtered_state = filtered_state.filter((item) => {
+            return item.aspects!.some(({ id }) => id === filters.aspect)
+        })
+    }
+    return filtered_state
+}
+
+function ItemsReducer(
+    state: ItemsFromQuery[],
+    action: ItemsAction,
+): ItemsFromQuery[] {
+    switch (action.type) {
+        case "filter": {
+            const { filters } = action
+            return filterItems(filters, state)
+        }
+    }
+}
 
 function Item({ ...item }: ItemFromQuery) {
-    const principles = [
-        "edge",
-        "forge",
-        "grail",
-        "heart",
-        "knock",
-        "lantern",
-        "moon",
-        "moth",
-        "nectar",
-        "rose",
-        "scale",
-        "sky",
-        "winter",
-    ]
     return (
         <Card key={item.id}>
             <CardHeader title={item.id} />
-            <CardContent>
-                {item.aspects!.map(({ id }) => (
-                    <Aspect key={id} id={id} />
-                ))}
-            </CardContent>
-            <CardActions>
+            <Stack direction={"row"} sx={{ maxWidth: "auto" }}>
                 {principles.map((principle) => {
                     if (item[principle] !== null)
                         return (
@@ -73,26 +127,31 @@ function Item({ ...item }: ItemFromQuery) {
                             />
                         )
                 })}
-            </CardActions>
+                {item.aspects!.map(({ id }) => (
+                    <Aspect key={id} id={id} />
+                ))}
+            </Stack>
         </Card>
     )
 }
-const ItemsView = () => {
+const ItemsView = ({ filters }: ItemsProps) => {
     const [{ data }] = useQuery({ query: itemsQueryDocument })
+
+    const [state, dispatch] = useReducer(
+        ItemsReducer,
+        filterItems({ known: true, ...filters }, data!.item),
+    )
+
     return (
         <Stack
             spacing={2}
             sx={{
-                maxWidth: "450px",
+                maxWidth: "auto",
                 marginBlock: 1,
                 marginInline: "auto",
             }}
         >
-            {data!.item
-                .filter(({ known }) => known === true)
-                .map((item) => (
-                    <Item key={item.id} {...item} />
-                ))}
+            {state?.map((item) => <Item key={item.id} {...item} />)}
         </Stack>
     )
 }
