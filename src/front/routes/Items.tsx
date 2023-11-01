19cd1c1fd18f632fb7d4aa8db8ff723c6f864814
Desiree Davison <ded8393@gmail.com>
2023-11-01 20:27:58 +0100

    Shopping List (#31)
    Co-authored-by: Phil Schaf <flying-sheep@web.de>

--- src/front/routes/Items.tsx
+++ src/front/routes/Items.tsx
@@ -1,11 +1,28 @@
-import { useMemo } from "react"
+import {
+    RefObject,
+    forwardRef,
+    memo,
+    useCallback,
+    useMemo,
+    useReducer,
+    useRef,
+    useState,
+} from "react"
 import { useQuery } from "urql"
 
+import Divider from "@mui/material/Divider"
+import Drawer from "@mui/material/Drawer"
 import List from "@mui/material/List"
-import ListItemButton from "@mui/material/ListItemButton"
+import ListItem from "@mui/material/ListItem"
+import ListItemButton, {
+    ListItemButtonProps,
+} from "@mui/material/ListItemButton"
+import ListItemIcon from "@mui/material/ListItemIcon"
 import ListItemText from "@mui/material/ListItemText"
 import Stack from "@mui/material/Stack"
 import Typography from "@mui/material/Typography"
+
+import Delete from "@mui/icons-material/Delete"
 
 import { graphql } from "../gql"
 import * as types from "../gql/graphql"
@@ -125,26 +142,51 @@ const ItemValues = ({ aspects, ...item }: ItemFromQuery) => (
                 />
             ),
         )}
-        {<AspectIconGroup aspects={aspects} />}
+        <AspectIconGroup aspects={aspects} />
     </Stack>
 )
 
-function Item({ ...item }: ItemFromQuery) {
-    return (
-        <ListItemButton>
-            <ListItemText primary={item.id} />
-            <ItemValues {...item} />
-        </ListItemButton>
-    )
+export interface ItemProps extends ItemFromQuery {
+    onToggleSelect?(id: string, selected: boolean): void
+    sx?: ListItemButtonProps["sx"] | undefined
 }
-const ItemsView = ({ filters }: ItemsProps) => {
-    const [{ data }] = useQuery({ query: itemsQueryDocument })
 
-    const state = useMemo(
-        () => filterItems(data!.item, { known: true, ...filters }),
-        [data, filters],
+const Item = forwardRef<HTMLDivElement, ItemProps>(function Item(
+    { onToggleSelect, sx, ...item },
+    ref,
+) {
+    const [selected, setSelected] = useState(false)
+    const handleToggleSelect = useCallback(() => {
+        setSelected(!selected)
+        onToggleSelect?.(item.id, !selected)
+    }, [item.id, onToggleSelect, selected])
+    return (
+        <ListItem disablePadding>
+            <ListItemButton
+                ref={ref}
+                selected={selected}
+                onClick={handleToggleSelect}
+                sx={sx}
+            >
+                <ListItemText primary={item.id} />
+                <ItemValues {...item} />
+            </ListItemButton>
+        </ListItem>
     )
+})
 
+interface ItemsListProps {
+    items: ItemFromQuery[]
+    itemRefs: RefObject<Map<string, RefObject<HTMLDivElement>>>
+    onToggleSelect(id: string, selected: boolean): void
+}
+
+// This list is long, use memo to prevent rerendering
+const ItemsList = memo(function ItemsList({
+    items,
+    itemRefs,
+    onToggleSelect,
+}: ItemsListProps) {
     return (
         <List
             sx={{
@@ -152,12 +194,126 @@ const ItemsView = ({ filters }: ItemsProps) => {
                 marginInline: "auto",
             }}
         >
-            {state
-                .filter(({ isVisible }) => isVisible)
-                .map((item) => (
-                    <Item key={item.id} {...item} />
-                ))}
+            {items.map((item) => (
+                <Item
+                    key={item.id}
+                    ref={itemRefs.current?.get(item.id)}
+                    onToggleSelect={onToggleSelect}
+                    {...item}
+                />
+            ))}
         </List>
+    )
+})
+
+interface ItemsDrawerProps {
+    items: VisibleItem[]
+    itemRefs: RefObject<Map<string, RefObject<HTMLDivElement>>>
+    onClear?(): void
+}
+
+function ItemsDrawer({ items, itemRefs, onClear }: ItemsDrawerProps) {
+    return (
+        <Drawer
+            variant="persistent"
+            open={items.length > 0}
+            sx={{
+                maxWidth: "250px",
+                "& .MuiDrawer-paper": { maxWidth: "250px" },
+            }}
+        >
+            <List sx={{ flexGrow: 1 }}>
+                {items.map((item) => (
+                    <ListItem key={item.id} disablePadding>
+                        <ListItemButton
+                            onClick={() =>
+                                itemRefs.current
+                                    ?.get(item.id)
+                                    ?.current?.scrollIntoView({
+                                        behavior: "smooth",
+                                    })
+                            }
+                        >
+                            <ListItemText>{item.id}</ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+                <Divider />
+                {onClear && (
+                    <ListItem disablePadding>
+                        <ListItemButton onClick={onClear}>
+                            <ListItemIcon>
+                                <Delete />
+                            </ListItemIcon>
+                            <ListItemText>Clear</ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+                )}
+            </List>
+        </Drawer>
+    )
+}
+
+type StringSetAction =
+    | { type: "clear" }
+    | { type: "toggle"; id: string; selected: boolean }
+
+function reduceStringSet(
+    state: Set<string>,
+    action: StringSetAction,
+): Set<string> {
+    switch (action.type) {
+        case "clear":
+            return new Set()
+        case "toggle": {
+            const nextState = new Set(state)
+            if (action.selected) nextState.add(action.id)
+            else nextState.delete(action.id)
+            return nextState
+        }
+    }
+}
+
+const ItemsView = ({ filters }: ItemsProps) => {
+    const [{ data }] = useQuery({ query: itemsQueryDocument })
+    const items = useMemo(
+        () =>
+            filterItems(data!.item, { known: true, ...filters }).filter(
+                ({ isVisible }) => isVisible,
+            ),
+        [data, filters],
+    )
+    // all possible item refs have a key, even if they’re filtered out
+    const itemRefs = useRef(
+        new Map<string, RefObject<HTMLDivElement>>(
+            data!.item.map(({ id }) => [id, { current: null }]),
+        ),
+    )
+    const [selected, dispatch] = useReducer(reduceStringSet, new Set<string>())
+    const clearSelected = useCallback(() => dispatch({ type: "clear" }), [])
+    const toggleSelected = useCallback(
+        (id: string, selected: boolean) =>
+            dispatch({ type: "toggle", id, selected }),
+        [],
+    )
+    const selectedItems = useMemo(
+        () => items.filter(({ id }) => selected.has(id)),
+        [items, selected],
+    )
+    return (
+        <>
+            <ItemsList
+                items={items}
+                itemRefs={itemRefs}
+                onToggleSelect={toggleSelected}
+            />
+            <ItemsDrawer
+                items={selectedItems}
+                itemRefs={itemRefs}
+                // TODO: this doesn’t work yet since the items manage their selected state themselves
+                onClear={undefined && clearSelected}
+            />
+        </>
     )
 }
 export default ItemsView
